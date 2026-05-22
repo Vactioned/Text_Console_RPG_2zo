ff677c2a8209207094f43db98914acaef5c53651
sim6579 <wldnjs6579@gmail.com>
2026-05-22 14:03:54 +0900

    [Feat] 레벨업 기능 구현 ( #16 )

--- Text_Console_RPG_2zo/Text_Console_RPG_2zo/LevelUp.cpp
+++ Text_Console_RPG_2zo/Text_Console_RPG_2zo/LevelUp.cpp
@@ -1,1 +1,25 @@
-﻿
+﻿#include <iostream>
+#include "LevelUp.h"
+
+using namespace std;
+
+void LevelUp::CheckLevelUp(Player* player)
+{
+    while (player -> getExp() >= player -> getmaxExp())
+    {
+        player -> setExp(player -> getExp() - player->getmaxExp());
+        player -> setLevel(player -> getLevel() + 1);
+    }
+}
+
+void LevelUp::AddExp(Player* player, int amount)
+{
+    player -> setExp(player->getExp() + amount);
+    CheckLevelUp(player);
+}
+
+void LevelUp::PrintLevelInfo(Player* player)
+{
+    cout << "Level : " << player -> getLevel() << endl;
+    cout << "Exp : " << player -> getExp() << " / " << player -> getmaxExp() << endl;
+}
