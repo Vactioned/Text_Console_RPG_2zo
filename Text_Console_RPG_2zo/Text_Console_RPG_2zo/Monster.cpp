871411508aeb20ee2bd99f4e3dbc70c7f218f945
Jaeseong <wntks147@gmail.com>
2026-05-22 12:22:32 +0900

    [Feat] 몬스터 기본 구조 및 스탯 정의 ( #13 )

---
+++ Text_Console_RPG_2zo/Text_Console_RPG_2zo/Monster.cpp
@@ -0,0 +1,36 @@
+﻿#include "Monster.h"
+#include "Player.h"
+#include <iostream>
+#include <string>
+
+Monster::Monster(int maxhp, int attackPower, int SANPower) {
+    this->maxhp = maxhp;
+    this->hp = maxhp;
+    this->attackPower = attackPower;
+    this->SANPower = SANPower;
+}
+
+void Monster::setname(std::string name) {
+    this->name = name;
+}
+
+void Monster::TakeDamage(int damage) {
+    hp -= damage;
+    if (hp < 0) hp = 0;
+
+    std::cout << name <<"(이)가" << damage << "의 피해를 입었습니다.";
+}
+void Monster::Attack() {
+
+    std::cout << name << "(이)가" << attackPower << "의 피해를 입혔습니다!";
+}
+
+//정신력 공격 구현
+void Monster::SanAttack(Player* player, int SanDamage) {
+
+    std::cout << name << "정신을 오염시켰습니다.";
+
+    if (player != nullptr) {
+       // player-> 정신데미지 함수삽입
+    }
+}
---
+++ Text_Console_RPG_2zo/Text_Console_RPG_2zo/Monster.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <iostream>
+#include <string> 
+
+class Player;
+
+class Monster {
+private:
+    int hp;
+    int maxhp;
+    int attackPower;
+    int SANPower;
+    std::string name; 
+
+public:
+   
+    Monster(int maxhp, int attackPower, int SANPower);
+
+    void TakeDamage(int damage);  //몬스터가 받는 데미지
+    void Attack(); // 몬스터가 주는 데미지
+
+    
+    void SanAttack(Player* player, int SanDamage); //몬스터가 플레이어에게 주는 정신공격데미지
+
+    void setname(std::string name);
+
+   
+    int GetHp() { return hp; }
+    int GetMaxhp() { return maxhp; }
+    int GetAttackPower() { return attackPower; }
+    int GetSANPower() { return SANPower; }
+
+    std::string Getname() { return name; } 
+};
