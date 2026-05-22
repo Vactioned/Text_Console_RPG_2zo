3a568982f07c96ceda5287575ed296e45e51bdd0
1dy1929 <djsfldjf1929@gmail.com>
2026-05-22 18:22:55 +0900

    [Feat] 플레이어 아이템과 사용 기능 구현 #41

--- Text_Console_RPG_2zo/Text_Console_RPG_2zo/Item.h
+++ Text_Console_RPG_2zo/Text_Console_RPG_2zo/Item.h
@@ -1,8 +1,34 @@
 ﻿#pragma once
 
+#include <string>
+
+class Player;
+
+enum class ItemType
+{
+    HealPotion,
+    SANPotion,
+    AttackBoost
+};
+
 class Item
 {
+private:
+    std::string name;
+    ItemType type;
+    int value;
+
 public:
-    Item();
-    ~Item();
+    Item(const std::string& name, ItemType type, int value);
+
+    std::string getName() const;
+    ItemType getType() const;
+    int getValue() const;
+
+    // Player& 대신 Player*를 받도록 수정
+    void use(Player* player) const;
+
+    static Item createHealPotion();
+    static Item createSANPotion();
+    static Item createAttackBoost();
 };
