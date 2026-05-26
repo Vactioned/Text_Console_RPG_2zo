64655d46b81c91ebd76e9b1f7c54892bc64008d2
Jaeseong <wntks147@gmail.com>
2026-05-26 22:39:36 +0900

    [Feat] (1막 몬스터) 각기 다른 데미지로직 몬스터 헤더 생성 ( #115 )

---
+++ Text_Console_RPG_2zo/Text_Console_RPG_2zo/VoidHound.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include "Monster.h"
+#include "Player.h"
+#include <cstdlib>
+
+class VoidHound : public Monster {
+public:
+    VoidHound(int playerlevel) //코드 간략화
+        :Monster(rand() % (playerlevel * 10 + 1) + (playerlevel * 20), // 체력
+            rand() % (playerlevel * 5 + 1) + (playerlevel * 5),  // 공격력
+            0, // 정신공격력
+            5, // 몬스터 방어력
+            "공허의 파수견") // 몬스터이름
+
+    {
+    }
+
+    void NormalAttack(Player* player) override {
+        std::cout << getName() << "이 날카로운 이빨로 물어뜯습니다!" << std::endl;
+        std::cout << "->" << getAttackPower() << "의 물리 피해를 입혔습니다!" << std::endl;
+
+        if (player != nullptr) {
+
+        }
+    }
+
+    void SpecialAttack(Player* player) override {
+        int initialdamage = getAttackPower()* 1.5; // 즉시 들어가는 타격 피해
+        int buffamount = 4;                // 증가할 공격력수치
+                   
+
+        std::cout << getName() << "이 붉은 눈을 드러내며 깊숙이 상처를 냅니다![잔혹한 할퀴기]" << std::endl;;
+        std::cout << "->" << initialdamage << "의 물리 피해를 입혔습니다!" << std::endl;
+
+        setAttackPower(getAttackPower() + buffamount);
+        std::cout << getName() << "의 이빨이 공허 에너지로 강화되어 공격력이" << buffamount << "영구 증가합니다!" << std::endl;
+       
+
+        if (player != nullptr) {
+            
+        }
+    }
+};
+
