42a326d5fd8a3256615983f2f4fae1346693c510
Jaeseong <wntks147@gmail.com>
2026-05-26 16:34:37 +0900

    [Feat] Monster 클래스 모든 스탯 setter 함수 추가 ( #106 )

--- Text_Console_RPG_2zo/Text_Console_RPG_2zo/Monster.h
+++ Text_Console_RPG_2zo/Text_Console_RPG_2zo/Monster.h
@@ -23,20 +23,37 @@ public:
 
     virtual void SpecialAttack(Player* player) = 0; // 2. 특수공격
 
-    void SetName(std::string name); //이름변경용 함수
+     
 
     void setHp(int newhp) {
         this->hp = newhp;
-
-        if (this->hp < 0) {
-            this->hp = 0;
-        }
-
-        if (this->hp > maxhp) {
-            this->hp = maxhp;
-        }
+        if (this->hp < 0) this->hp = 0;
+        if (this->hp > maxhp) this->hp = maxhp;
+      
     }
-    
+
+    void setMaxHp(int newmaxhp) {
+        this->maxhp = newmaxhp;
+        if (this->maxhp < 1) this->maxhp = 1;
+    }
+    void setAttackPower(int newattackpower) {
+        this->attackpower = newattackpower;
+        if (this->attackpower < 0) this->attackpower = 0;
+    }
+
+    void setSanPower(int newsanpower) {
+        this->sanpower = newsanpower;
+        if (this->sanpower < 0) this->sanpower = 0;
+    }
+
+    void setDefence(int newdefence) {
+        this->defence = newdefence;
+        if (this->defence < 0) this->defence = 0;
+    }
+
+    void SetName(std::string name) {
+        this->name = name;
+    }
 
    
     int getHp() { return hp; }
