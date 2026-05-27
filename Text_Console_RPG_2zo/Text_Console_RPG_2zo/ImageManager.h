e7be3c91db8911d063587faca41d6d13796a892e
sim6579 <wldnjs6579@gmail.com>
2026-05-27 16:54:07 +0900

    [Feat] 이미지 출력 함수 구현 ( #152 )

---
+++ Text_Console_RPG_2zo/Text_Console_RPG_2zo/ImageManager.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+
+
+void PrintImage(const std::string& fileName)
+{
+    sf::Texture image;
+    if (!image.loadFromFile(fileName))
+    {
+        std::cout << "이미지 로드 실패\n";
+        return;
+    }
+
+    std::string name = fileName;
+    std::string title = name.erase(0, 6);
+
+    sf::Vector2u size = image.getSize();
+
+    sf::RenderWindow window(
+        sf::VideoMode(size.x, size.y),
+        title,
+        sf::Style::Titlebar | sf::Style::Close
+    );
+
+    sf::Sprite imageSprite;
+    imageSprite.setTexture(image);
+    
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color(0, 0, 0));
+        window.draw(imageSprite);
+        window.display();
+    }
+}
