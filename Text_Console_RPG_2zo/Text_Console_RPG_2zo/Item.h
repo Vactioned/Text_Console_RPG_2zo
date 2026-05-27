92699f2bfffebe9d61432b62b2d74694c804d130
1dy1929 <djsfldjf1929@gmail.com>
2026-05-27 17:49:04 +0900

    [Feat] 아이템 일반 및 챕터 보상 분리 ( #158 )

--- Text_Console_RPG_2zo/Text_Console_RPG_2zo/Item.h
+++ Text_Console_RPG_2zo/Text_Console_RPG_2zo/Item.h
@@ -6,18 +6,15 @@ using namespace std;
 
 class Player;
 
-// 아이템 종류
 enum class ItemType
 {
-    HealPotion,          // 체력 회복 포션
-    SanPotion,           // 정신력 회복 포션    
-    AttackBoost,         // 공격력 증가 (이번 전투만)     
-
-    WeaknessPotion,      // 적의 공격력 감소             (미구현)
-    VulnerabilityPotion, // 적에게 가하는 피해 증가       (미구현)
-
-    MaxHpPotion,         // 최대 체력 영구 증가
-    MaxSanPotion         // 최대 정신력 영구 증가
+    HealPotion,           // 체력 회복 포션
+    SanPotion,            // 정신력 회복 포션
+    AttackBoost,          // 공격력 증가 포션
+    WeaknessPotion,       // 약화 포션
+    VulnerabilityPotion,  // 취약 포션
+    MaxHpPotion,          // 최대 체력 증가 포션
+    MaxSanPotion          // 최대 정신력 증가 포션
 };
 
 class Item
@@ -28,18 +25,14 @@ private:
     int value;
 
 public:
-    // 생성자
+    Item();
     Item(const string& name, ItemType type, int value);
 
-    // getter
     string getName() const;
     ItemType getType() const;
     int getValue() const;
 
-    // 아이템 정보 출력
     void PrintInfo() const;
-
-    // 아이템 사용
     void use(Player* player) const;
 
     // 아이템 생성 함수
@@ -48,8 +41,12 @@ public:
     static Item CreateAttackBoost();
     static Item CreateWeaknessPotion();
     static Item CreateVulnerabilityPotion();
-
-    // 영구 증가 포션
     static Item CreateMaxHpPotion();
     static Item CreateMaxSanPotion();
+
+    // 일반 전투 보상 (30% 확률, 체력/정신력 포션)
+    static bool CreateBattleReward(Item& reward);
+
+    // 챕터 클리어 보상 (5개 중 랜덤 지급)
+    static Item CreateChapterClearReward();
 };
