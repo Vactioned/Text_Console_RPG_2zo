774ceeeeea96266eb53996dc0f23fdd61e671e4d
ohsun2728 <ohsun2728@gmail.com>
2026-05-28 19:10:54 +0900

    [Refactor] 직업 선택 시 대사 출력 ( #191 )

--- Text_Console_RPG_2zo/Text_Console_RPG_2zo/LogManager.cpp
+++ Text_Console_RPG_2zo/Text_Console_RPG_2zo/LogManager.cpp
@@ -172,24 +172,81 @@ string LogManager::SelectJob()              // 직업 입력 함수
         cout << " [선택 1] : 버서커" << endl;
         cout << " [선택 2] : 흑마법사" << endl;
         cout << " [선택 3] : 도적" << endl;
-        cout << " [선택 4] : 수도승" << endl;
+        cout << " [선택 4] : 파계승" << endl;
         cout << " [선택 5] : 도파민 상자 (당신의 운을 시험하세요)" << endl;
+        cout << "================================================================" << endl;
         cout << "입력 : ";
         cin >> choice;
+        cout << endl;
 
         switch (choice)
         {
-        case 1: job = "Berserker"; break;
-        case 2: job = "Warlock"; break;
-        case 3: job = "Thief"; break;
-        case 4: job = "Monk"; break;
+        case 1: job = "Berserker";
+            cout << "<광전사>" << endl;
+            cout << "\"닥치고 피나 쏟아내라. 이 미친 갈증을 채우려면 멀었어.\"" << endl;
+            cout << "자신의 살점이 떨어져 나가는 고통마저 살육의 희열로 바꾸는 광전사" << endl;
+            break;
+        case 2: job = "Warlock";
+            cout << "<흑마법사>" << endl;
+            cout << "\"세상이 금기라고 부르는 것들? 나한테는 다 힘일 뿐이야.\"" << endl;
+            cout << "금단의 지식을 탐한 대가로 강력한 파괴의 권능을 얻은 자" << endl;
+            break;
+        case 3: job = "Thief";
+            cout << "<도적>" << endl;
+            cout << "\"영웅 놀이는 딴 데 가서 해. 난 내 몫 챙겨서 살 궁리나 할 테니까.\"" << endl;
+            cout << "혼란을 틈타 오직 자신만의 생존과 이득을 취하는 무법자" << endl;
+            break;
+        case 4: job = "Monk";
+            cout << "<파계승>" << endl;
+            cout << "\"계율이 밥 먹여 줍니까? 살길은 내가 알아서 만듭니다.\"" << endl;
+            cout << "종교의 규율을 깨부수고 독자적인 정신의 경지에 이른 자" << endl;
+            break;
         case 5:
         {
             srand(static_cast<unsigned int>(time(NULL)));
 
-            // 1~100 사이의 난수를 발생
-            int chance = (rand() % 100) + 1;
-            job = (chance <= 80) ? "Poor" : "Gambler";
+            // 1~6 사이의 난수를 발생 (총 6개의 모든 직업)
+            int randomRoll = (rand() % 6) + 1;
+
+            cout << "\n[도파민 상자 개방!] 주사위가 굴러갑니다... 결과는?! \n" << endl;
+
+            switch (randomRoll)
+            {
+            case 1: job = "Berserker";
+                cout << "<광전사>" << endl;
+                cout << "\"닥치고 피나 쏟아내라. 이 미친 갈증을 채우려면 멀었어.\"" << endl;
+                cout << "자신의 살점이 떨어져 나가는 고통마저 살육의 희열로 바꾸는 광전사" << endl;
+                break;
+            case 2: job = "Warlock";
+                cout << "<흑마법사>" << endl;
+
+                cout << "\"세상이 금기라고 부르는 것들? 나한테는 다 힘일 뿐이야.\"" << endl;
+                cout << "금단의 지식을 탐한 대가로 강력한 파괴의 권능을 얻은 자" << endl;
+                break;
+            case 3: job = "Thief";
+                cout << "<도적>" << endl;
+                cout << "\"영웅 놀이는 딴 데 가서 해. 난 내 몫 챙겨서 살 궁리나 할 테니까.\"" << endl;
+                cout << "혼란을 틈타 오직 자신만의 생존과 이득을 취하는 무법자" << endl;
+                break;
+            case 4: job = "Monk";
+                cout << "<파계승>" << endl;
+                cout << "\"계율이 밥 먹여 줍니까? 살길은 내가 알아서 만듭니다.\"" << endl;
+                cout << "종교의 규율을 깨부수고 독자적인 정신의 경지에 이른 자" << endl;
+                break;
+            case 5:
+                job = "Poor";
+                cout << "<부랑자>" << endl;
+                cout << "\"배가 너무 고파요... 먹을 것 좀 나눠주십쇼...\"" << endl;
+                cout << "인생의 바닥에서 하루하루 간신히 살아가는 비참한 부랑자" << endl;
+                break;
+
+            case 6:
+                job = "Gambler";
+                cout << "<갬블러(히든)>" << endl;
+                cout << "\"너 그거 중독이야!\"" << endl;
+                cout << "확률의 신을 믿으며 모든 것을 판돈으로 거는 위험한 도박꾼" << endl;
+                break;
+            }
             break;
         }
         default:
